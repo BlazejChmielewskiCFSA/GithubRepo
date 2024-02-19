74605ddfa00ab712d5aae9260ca53aa0535e1107
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-02-19 14:18:46 +0100

    Tworze logikę obsługi błedu

--- src/main/java/pl/chmielewski/GithubApi/Exception/UserNotFoundException.java
+++ src/main/java/pl/chmielewski/GithubApi/Exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package pl.chmielewski.GithubApi.Exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String username) {
-        super("User with username " + username + " does not exist");
+        super("User with username '" + username + "' does not exist");
     }
 }
