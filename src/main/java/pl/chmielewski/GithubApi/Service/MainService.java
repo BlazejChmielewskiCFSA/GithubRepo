3ae00a8229ba738edf7a72ac62eb8a4e43be2c26
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-02-19 14:19:41 +0100

    Tworze pobieranie danych z Api githuba

--- src/main/java/pl/chmielewski/GithubApi/Service/MainService.java
+++ src/main/java/pl/chmielewski/GithubApi/Service/MainService.java
@@ -1,20 +1,24 @@
 package pl.chmielewski.GithubApi.Service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import pl.chmielewski.GithubApi.Model.GithubRepo;
+import pl.chmielewski.GithubApi.Exception.UserNotFoundException;
+import pl.chmielewski.GithubApi.Model.GitHubUser;
 
 @Service
 public class MainService {
 
-    RestTemplate restTemplate = new RestTemplate();
-
-    public MainService(String username) {
-        GithubRepo[] repositories = restTemplate.getForObject("https://api.github.com/users/" + username + "/repos", GithubRepo[].class);
-
-        assert repositories != null;
-        for (GithubRepo repository : repositories) {
-            repository.getOwner();
+    public GitHubUser getUsername(String username) {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "https://api.github.com/users/" + username;
+        try {
+            GitHubUser user = restTemplate.getForObject(apiUrl, GitHubUser.class);
+            return ResponseEntity.ok(user).getBody();
+        } catch (
+                HttpClientErrorException.NotFound e) {
+            throw new UserNotFoundException(username);
         }
     }
 }
