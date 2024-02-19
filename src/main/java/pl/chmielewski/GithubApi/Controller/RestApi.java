c4edc9f6651fb14a1da2417e02ae634d65ea374e
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-02-19 14:19:36 +0100

    Tworze pobieranie danych z Api githuba

--- src/main/java/pl/chmielewski/GithubApi/Controller/RestApi.java
+++ src/main/java/pl/chmielewski/GithubApi/Controller/RestApi.java
@@ -1,4 +1,25 @@
 package pl.chmielewski.GithubApi.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.chmielewski.GithubApi.Model.GitHubUser;
+import pl.chmielewski.GithubApi.Service.MainService;
+
+
+@RestController
+@RequestMapping("/api")
 public class RestApi {
+
+    private final MainService mainService;
+
+    public RestApi(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    @GetMapping("/user")
+    public GitHubUser getUser(@RequestParam String username) {
+        return mainService.getUsername(username);
+    }
 }
