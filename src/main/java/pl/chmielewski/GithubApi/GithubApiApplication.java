c67ae5f43110136aae1345480baf16f840c54c27
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-02-21 13:42:14 +0100

    Dodaje adnotacje pozwajacą pracować swaggerowi

--- src/main/java/pl/chmielewski/GithubApi/GithubApiApplication.java
+++ src/main/java/pl/chmielewski/GithubApi/GithubApiApplication.java
@@ -2,8 +2,10 @@ package pl.chmielewski.GithubApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class GithubApiApplication {
 	public static void main(String[] args){
 		SpringApplication.run(GithubApiApplication.class, args);
