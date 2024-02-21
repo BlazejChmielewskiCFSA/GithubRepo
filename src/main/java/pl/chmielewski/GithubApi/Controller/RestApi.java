075fa52b9ca7070226961a2a27f3441bdef5be74
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-02-21 13:11:02 +0100

    Wystawiam endpoint pobierający główną metodę

--- src/main/java/pl/chmielewski/GithubApi/Controller/RestApi.java
+++ src/main/java/pl/chmielewski/GithubApi/Controller/RestApi.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.chmielewski.GithubApi.Model.GitHubUser;
+import pl.chmielewski.GithubApi.Model.GithubUser;
 import pl.chmielewski.GithubApi.Service.MainService;
 
 
@@ -18,8 +18,13 @@ public class RestApi {
         this.mainService = mainService;
     }
 
-    @GetMapping("/user")
-    public GitHubUser getUser(@RequestParam String username) {
+    @GetMapping("/test")
+    public GithubUser getUser(@RequestParam String username) {
         return mainService.getUsername(username);
+    }
+
+    @GetMapping("/user")
+    public String buildJSON(@RequestParam String username) {
+        return mainService.buildJSON(username);
     }
 }
