2b5a07220a4d1d17bf005a2c1284f151ce753797
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-02-23 07:19:01 +0100

    Dodaje potrzebne komponenty do swaggera

--- src/main/java/pl/chmielewski/GithubApi/GithubApiApplication.java
+++ src/main/java/pl/chmielewski/GithubApi/GithubApiApplication.java
@@ -2,12 +2,19 @@ package pl.chmielewski.GithubApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-@EnableSwagger2
 public class GithubApiApplication {
 	public static void main(String[] args){
 		SpringApplication.run(GithubApiApplication.class, args);
 	}
+
+//	@Bean
+//	public Docket get(){
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.paths(PathSelectors.ant("/api/**"))
+//				.build();
+//	}
 }
